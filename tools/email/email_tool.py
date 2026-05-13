b28f7f494e170004d3228b53a7cce9a9eac328dd
Dmytro <dmytrodanylov@ukr.net>
2026-05-13 17:51:19 +0300

    Created email_tool.py

---
+++ tools/email/email_tool.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 17:24:30 2026
+
+@author: Dmytro
+"""
+import smtplib
+from email.message import EmailMessage
+from langchain.tools import Tool
+import os
+
+def send_email(receiver: str, subject: str, content: str):
+    message = EmailMessage()
+    message['To'] = receiver
+    message['Subject'] = subject
+    message.set_content(content)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(os.environ['SMTP_EMAIL_ADDRESS'], 
+                   os.environ['SMTP_EMAIL_PASS'])
+        smtp.send_message(message)
+        
+email_tool = Tool(name = 'email_sender', func = send_email, 
+                  description = 'Sends an email.')
