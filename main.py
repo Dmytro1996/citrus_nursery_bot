f04338f424e40d157c1a97f53f93c8554b996bfb
Dmytro <dmytrodanylov@ukr.net>
2026-06-11 17:52:23 +0300

    Replaced llm with an agent inside TelegramAIBot class

--- main.py
+++ main.py
@@ -6,26 +6,27 @@ Created on Thu May  7 18:08:12 2026
 """
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
-#from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 #from langchain_chroma import Chroma
 #from langchain_core.runnables import RunnablePassthrough
 #from langchain_core.output_parsers import StrOutputParser
+from langchain_classic.agents import create_tool_calling_agent, AgentExecutor
 import os
 import nest_asyncio
 import sys
 sys.path.insert(1, os.getcwd() + '/tools/email')
-sys.path.insert(1, os.getcwd() + '/tools/email')
+sys.path.insert(1, os.getcwd() + '/tools/db')
 from email_tool import email_tool
 from db_tool import citrus_search_tool, ordering_tool
 
 class TelegramAIBot:
     
-    def __init__(self, auth_token: str, llm: ChatHuggingFace, prompt: str):
+    def __init__(self, auth_token: str, agent: AgentExecutor):
         self.app = Application.builder().token(auth_token).build()
         self.app.add_handler(MessageHandler(filters.TEXT, self.process_message))
-        self.llm = llm
-        self.prompt = prompt
+        self.agent = agent
+        #self.prompt = prompt
         self.conversation_history = {}
         
     def start_polling(self):
@@ -45,26 +46,30 @@ class TelegramAIBot:
             self.conversation_history[chat_id].pop(0)
             self.conversation_history[chat_id].pop(0)
         print(self.conversation_history[chat_id])
-        response = self.llm.invoke(prompt.format(
-            '\n'.join(self.conversation_history[chat_id]), message)).content
+        response = self.agent.invoke({"input" : "\n".join(self.conversation_history[chat_id] + [message])})['output']
         print('User\'s message:\n' + message)
         print('LLM\'s response:\n' + response)
         await update.message.reply_text(response)
         self.conversation_history[chat_id].append('Clementine: ' + response)
 
-prompt = """Your name is Clementine and you are a telegram chatbot, 
-that assists customers of a citrus nursery with choosing a citrus tree. 
-You are only going to be asked questions about citruses, if you are asked 
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """Your name is Clementine and you are a Telegram chatbot, 
+that assists customers of a citrus nursery with choosing and ordering a citrus 
+tree. You are only going to be asked questions about citruses, if you are asked 
 something different ignore such question by simply replying that you only answer 
 questions about citruses. If you receive /start message greet the customer, 
 introduce yourself and offer help, if you receive /help message say that you're 
 here to assist with choosing a citrus tree and ask about which citrus tree a 
-customer wants.Here are the last messages from conversation for you to understand
-context:{}. Try to keep your generated messages as short as possible, 
+customer wants.Use  citrus_search_tool to search for citruses, that are 
+for sale in a nursery. If customer wants to make an order, ask him for his name, 
+email and citrus trees he wants to order, then create an order use ordering_tool. 
+After creating an order send an order confirmation email to a customer with his 
+order id, using email_tool. 
+Try to keep your generated messages as short as possible, 
 especially replies for /start and /help, remember - it's a chat in Telegram.
-Reply to a message specified below. 
-Message: {}
-"""
+"""),
+    ("human", "{input}"),
+    ("placeholder", "{agent_scratchpad}")])
 
 llm = ChatHuggingFace(llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2",
                      #repo_id="HuggingFaceH4/zephyr-7b-alpha"
@@ -77,8 +82,12 @@ llm = ChatHuggingFace(llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-In
 #result = llm.invoke(prompt.format('I can\'t choose between Valencia and Washington Navel orange...'))
 #print(result.content)
 print(type(email_tool))
+tools = [email_tool, citrus_search_tool, ordering_tool]
+agent = create_tool_calling_agent(llm = llm, prompt = prompt, tools = tools)
+agent_executor = AgentExecutor(agent = agent, tools = tools, verbose = True)
 nest_asyncio.apply()
-telegram_ai_bot = TelegramAIBot(os.environ['TELEGRAM_AUTH_TOKEN'], llm, prompt)
+telegram_ai_bot = TelegramAIBot(os.environ['TELEGRAM_AUTH_TOKEN'], 
+                                agent_executor)
 telegram_ai_bot.start_polling()
 #def main():
 #    app = ApplicationBuilder().token(os.environ['TELEGRAM_AUTH_TOKEN']).build()
