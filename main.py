6327e880a3c2aec8fa69c4ced2ae4bbd1f4dc371
Dmytro <dmytrodanylov@ukr.net>
2026-06-13 03:13:47 +0300

    Replaced HuggingFace with Gemini and changed old tools to new ones

--- main.py
+++ main.py
@@ -7,18 +7,15 @@ Created on Thu May  7 18:08:12 2026
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
-#from langchain_chroma import Chroma
-#from langchain_core.runnables import RunnablePassthrough
-#from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_classic.agents import create_tool_calling_agent, AgentExecutor
 import os
 import nest_asyncio
 import sys
 sys.path.insert(1, os.getcwd() + '/tools/email')
 sys.path.insert(1, os.getcwd() + '/tools/db')
-from email_tool import email_tool
-from db_tool import citrus_search_tool, ordering_tool
+from email_tool import send_email
+from db_tool import search_citrus_trees, create_order
 
 class TelegramAIBot:
     
@@ -46,7 +43,9 @@ class TelegramAIBot:
             self.conversation_history[chat_id].pop(0)
             self.conversation_history[chat_id].pop(0)
         print(self.conversation_history[chat_id])
-        response = self.agent.invoke({"input" : "\n".join(self.conversation_history[chat_id] + [message])})['output']
+        response = self.agent.invoke({
+            "input" : "\n".join(self.conversation_history[chat_id])}
+            )['output'][0]['text']
         print('User\'s message:\n' + message)
         print('LLM\'s response:\n' + response)
         await update.message.reply_text(response)
@@ -60,37 +59,25 @@ something different ignore such question by simply replying that you only answer
 questions about citruses. If you receive /start message greet the customer, 
 introduce yourself and offer help, if you receive /help message say that you're 
 here to assist with choosing a citrus tree and ask about which citrus tree a 
-customer wants.Use  citrus_search_tool to search for citruses, that are 
+customer wants.Use  search_citrus_trees tool to search for citruses, that are 
 for sale in a nursery. If customer wants to make an order, ask him for his name, 
-email and citrus trees he wants to order, then create an order use ordering_tool. 
-After creating an order send an order confirmation email to a customer with his 
-order id, using email_tool. 
+email and citrus trees he wants to order, then create an order using create_order 
+tool. After creating an order send an order confirmation email to a customer 
+with his order id, using send_email tool. 
 Try to keep your generated messages as short as possible, 
 especially replies for /start and /help, remember - it's a chat in Telegram.
 """),
     ("human", "{input}"),
     ("placeholder", "{agent_scratchpad}")])
 
-llm = ChatHuggingFace(llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2",
-                     #repo_id="HuggingFaceH4/zephyr-7b-alpha"
-                     huggingfacehub_api_token = os.environ['HF_API_KEY'],
-                     temperature = 0.5,
-                     max_new_tokens = 512
-                     #provider = "featherless-ai",
-                     #task="conversational"
-                     ))
-#result = llm.invoke(prompt.format('I can\'t choose between Valencia and Washington Navel orange...'))
-#print(result.content)
-print(type(email_tool))
-tools = [email_tool, citrus_search_tool, ordering_tool]
+llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite", 
+                             api_key = os.environ['GOOGLE_GENAI_API_KEY'])
+#print(type(email_tool))
+#tools = [email_tool, citrus_search_tool, ordering_tool]
+tools = [send_email, search_citrus_trees, create_order]
 agent = create_tool_calling_agent(llm = llm, prompt = prompt, tools = tools)
 agent_executor = AgentExecutor(agent = agent, tools = tools, verbose = True)
 nest_asyncio.apply()
 telegram_ai_bot = TelegramAIBot(os.environ['TELEGRAM_AUTH_TOKEN'], 
                                 agent_executor)
 telegram_ai_bot.start_polling()
-#def main():
-#    app = ApplicationBuilder().token(os.environ['TELEGRAM_AUTH_TOKEN']).build()
-#    app.run_polling()
-#if(__name__ == '__main__'):
-#    main()
